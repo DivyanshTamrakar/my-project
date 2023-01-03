3ceef3c2de430cc92bb46337f2cace6064caa37f
Divyansh Tamrakar <divyanshtamrakar@Divyansh-Tamrakar.local>
2023-01-03 16:18:31 +0530

    fix:image component

--- components/ListItem/card.tsx
+++ components/ListItem/card.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import React from "react";
 
@@ -6,7 +5,14 @@ function ImageCard({ item }: any) {
   return (
     <div className="mt-2 flex justify-center cursor-pointer hover:mt-1 ease-out duration-300">
       <div className="rounded-lg shadow-lg bg-white max-w-sm">
-        <img className="rounded-t-lg " src={item.url} alt="something" />
+        <Image
+          className="w-auto h-auto object-cover rounded-t-md"
+          src={item.url}
+          alt="something"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
         <div className="p-6">
           <h5 className="text-meetupText text-xl font-bold mb-2 tracking-wide">
             {item.title}
